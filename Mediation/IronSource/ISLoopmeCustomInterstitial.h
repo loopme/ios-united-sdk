6ffd3b9f499a47634851780cd47357443f1d3ebc
NirfikLoopMe <69302815+NirfikLoopMe@users.noreply.github.com>
2022-01-13 10:45:07 +0200

    is adapter

---
+++ Mediation/IronSource/ISLoopmeCustomInterstitial.h
@@ -0,0 +1,19 @@
+//
+//  ISLoopmeCustomInterstitial.h
+//  IronSourceDemoApp
+//
+//  Created by Volodymyr Novikov on 14.12.2021.
+//  Copyright © 2021 supersonic. All rights reserved.
+//
+
+#ifndef ISLoopmeCustomInterstitial_h
+#define ISLoopmeCustomInterstitial_h
+
+#import "IronSource/ISBaseAdAdapter.h"
+#import "IronSource/ISAdapterErrors.h"
+
+@interface ISLoopmeCustomInterstitial : ISBaseAdAdapter
+
+@end
+
+#endif /* ISLoopmeCustomInterstitial_h */
