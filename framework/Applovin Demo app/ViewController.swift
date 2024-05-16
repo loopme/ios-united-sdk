7bfc39bfd2e807eb3985da447006261974ebabce
ValeriiLoopMe <valerii.roman@loopme.com>
2024-05-16 12:31:59 +0200

    Added new Target Applovin

---
+++ framework/Applovin Demo app/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Applovin Demo app
+//
+//  Created by Valerii Roman on 16/05/2024.
+//  Copyright © 2024 LoopMe. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
