3a1217cdf2e740a3e9c5f01eb0e68ff2952524f7
ValeriiLoopMe <valerii.roman@loopme.com>
2024-07-29 12:08:19 +0200

    [SDK-338] Add Latency logic  to IronSource demo app

---
+++ Mediation/IronSource/IronSource/IronSourceDemoApp/LetancyReportViewController.h
@@ -0,0 +1,18 @@
+//
+//  LetancyReportViewController.h
+//  IronSourceDemoApp
+//
+//  Created by Valerii Roman on 17/07/2024.
+//
+#import <UIKit/UIKit.h>
+
+#ifndef LetancyReportViewController_h
+#define LetancyReportViewController_h
+
+
+#endif /* LetancyReportViewController_h */
+
+@interface LetancyReportViewController : UIViewController 
+@property (weak, nonatomic) IBOutlet UITextView *latencyText;
+
+@end
