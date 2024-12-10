0b37147a68dfe7072d8a5e8106d4654fd802eb90
ValeriiLoopMe <valerii.roman@loopme.com>
2024-12-10 12:46:15 +0100

    [SDK-503] Added to telemetry compatibility to objective-c

--- framework/LoopMeUnitedSDK/Core/Internal/Telemetry/Model/TelemetryEventType.swift
+++ framework/LoopMeUnitedSDK/Core/Internal/Telemetry/Model/TelemetryEventType.swift
@@ -7,31 +7,109 @@
 
 import Foundation
 
-enum TelemetryEventType: String {
-    case sessionStart = "session_start"
-    case sessionEnd = "session_end"
-    case initializationDuration = "initialization_duration"
-    case adBidResponseDuration = "ad_bid_response_duration"
-    case adLoadDuration = "ad_load_duration"
-    case videoBufferingAverage = "video_buffering_average"
-    case adDisplayDuration = "ad_display_duration"
-    
-    var requiredAttributes: [EventAttribute] {
-        switch self {
-        case .sessionStart:
-            return [.session_id, .created_at, .mediation, .platform, .version, .adapter_version, .package]
-        case .sessionEnd:
-            return [.session_id, .created_at, .mediation, .platform, .version, .adapter_version, .package, .duration]
-        case .initializationDuration:
-            return [.session_id, .created_at, .mediation, .platform, .version, .adapter_version, .package, .duration]
-        case .adBidResponseDuration:
-            return [.session_id, .impression_id, .created_at, .mediation, .platform, .version, .adapter_version, .package, .duration]
-        case .adLoadDuration:
-            return [.session_id, .impression_id, .created_at, .mediation, .platform, .version, .adapter_version, .package, .duration]
-        case .videoBufferingAverage:
-            return [.session_id, .impression_id, .created_at, .mediation, .platform, .version, .adapter_version, .package, .duration_avg, .media_url]
-        case .adDisplayDuration:
-            return [.session_id, .impression_id, .created_at, .mediation, .platform, .version, .adapter_version, .package, .duration]
+@objc(TelemetryEventType)
+public class TelemetryEventType: NSObject {
+    @objc public static let sessionStart = "session_start"
+    @objc public static let sessionEnd = "session_end"
+    @objc public static let initializationDuration = "initialization_duration"
+    @objc public static let adBidResponseDuration = "ad_bid_response_duration"
+    @objc public static let adLoadDuration = "ad_load_duration"
+    @objc public static let videoBufferingAverage = "video_buffering_average"
+    @objc public static let adDisplayDuration = "ad_display_duration"
+
+    @objc public static func requiredAttributes(for eventType: String) -> NSMutableDictionary {
+        let attributes: [String]
+        switch eventType {
+            case sessionStart:
+                attributes = [
+                    EventAttribute.session_id,
+                    EventAttribute.created_at,
+                    EventAttribute.mediation,
+                    EventAttribute.platform,
+                    EventAttribute.sdk_version,
+                    EventAttribute.mediation_sdk_version,
+                    EventAttribute.adapter_version,
+                    EventAttribute.package
+                ]
+            case sessionEnd:
+                attributes = [
+                    EventAttribute.session_id,
+                    EventAttribute.created_at,
+                    EventAttribute.mediation,
+                    EventAttribute.platform,
+                    EventAttribute.versionNumber,
+                    EventAttribute.adapter_version,
+                    EventAttribute.package,
+                    EventAttribute.duration
+                ]
+            case initializationDuration:
+                attributes = [
+                    EventAttribute.session_id,
+                    EventAttribute.created_at,
+                    EventAttribute.mediation,
+                    EventAttribute.platform,
+                    EventAttribute.versionNumber,
+                    EventAttribute.adapter_version,
+                    EventAttribute.package,
+                    EventAttribute.duration
+                ]
+            case adBidResponseDuration:
+                attributes = [
+                    EventAttribute.session_id,
+                    EventAttribute.impression_id,
+                    EventAttribute.created_at,
+                    EventAttribute.mediation,
+                    EventAttribute.platform,
+                    EventAttribute.versionNumber,
+                    EventAttribute.adapter_version,
+                    EventAttribute.package,
+                    EventAttribute.duration
+                ]
+            case adLoadDuration:
+                attributes = [
+                    EventAttribute.session_id,
+                    EventAttribute.impression_id,
+                    EventAttribute.created_at,
+                    EventAttribute.mediation,
+                    EventAttribute.platform,
+                    EventAttribute.versionNumber,
+                    EventAttribute.adapter_version,
+                    EventAttribute.package,
+                    EventAttribute.duration
+                ]
+            case videoBufferingAverage:
+                attributes = [
+                    EventAttribute.session_id,
+                    EventAttribute.impression_id,
+                    EventAttribute.created_at,
+                    EventAttribute.mediation,
+                    EventAttribute.platform,
+                    EventAttribute.versionNumber,
+                    EventAttribute.adapter_version,
+                    EventAttribute.package,
+                    EventAttribute.duration_avg,
+                    EventAttribute.media_url
+                ]
+            case adDisplayDuration:
+                attributes = [
+                    EventAttribute.session_id,
+                    EventAttribute.impression_id,
+                    EventAttribute.created_at,
+                    EventAttribute.mediation,
+                    EventAttribute.platform,
+                    EventAttribute.versionNumber,
+                    EventAttribute.adapter_version,
+                    EventAttribute.package,
+                    EventAttribute.duration
+                ]
+            default:
+                attributes = []
+            }
+
+        let dictionary = NSMutableDictionary()
+        for key in attributes {
+            dictionary[key] = NSNull()
         }
+        return dictionary
     }
 }
